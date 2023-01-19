308c6b267812cbec56972fd1489a5c21ffe9610d
SeoJiWon <sjiwon4491@gmail.com>
2023-01-19 20:26:39 +0900

    test: PointDetail 도메인 테스트 코드

---
+++ src/test/java/com/sjiwon/anotherart/point/domain/PointDetailTest.java
@@ -0,0 +1,99 @@
+package com.sjiwon.anotherart.point.domain;
+
+import com.sjiwon.anotherart.fixture.MemberFixture;
+import com.sjiwon.anotherart.member.domain.Member;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class PointDetailTest {
+    private static final PasswordEncoder ENCODER = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    private static final MemberFixture MEMBER_A = MemberFixture.A;
+
+    @Test
+    @DisplayName("회원가입한 멤버의 포인트 내역")
+    void test1(){
+        // given
+        Member memberA = MEMBER_A.toMember(ENCODER);
+
+        // when
+        PointDetail pointDetail = PointDetail.createPointDetail(memberA);
+
+        // then
+        assertThat(pointDetail.getPointType()).isEqualTo(PointType.JOIN);
+        assertThat(pointDetail.getAmount()).isEqualTo(0);
+    }
+
+    @Test
+    @DisplayName("포인트 충전 후 멤버의 포인트 내역")
+    void test2(){
+        // given
+        Member memberA = MEMBER_A.toMember(ENCODER);
+
+        // when
+        final int chargeAmount = 10000;
+        PointDetail pointDetail = PointDetail.insertPointDetail(memberA, PointType.CHARGE, chargeAmount);
+
+        // then
+        assertThat(pointDetail.getPointType()).isEqualTo(PointType.CHARGE);
+        assertThat(pointDetail.getAmount()).isEqualTo(chargeAmount);
+        assertThat(memberA.getAvailablePoint().getValue()).isEqualTo(chargeAmount);
+    }
+
+    @Test
+    @DisplayName("포인트 환불 후 멤버의 포인트 내역")
+    void test3(){
+        // given
+        Member memberA = MEMBER_A.toMember(ENCODER);
+        final int increasePoint = 10000;
+        memberA.increasePoint(increasePoint);
+
+        // when
+        final int refundAmount = 5000;
+        PointDetail pointDetail = PointDetail.insertPointDetail(memberA, PointType.REFUND, refundAmount);
+
+        // then
+        assertThat(pointDetail.getPointType()).isEqualTo(PointType.REFUND);
+        assertThat(pointDetail.getAmount()).isEqualTo(refundAmount);
+        assertThat(memberA.getAvailablePoint().getValue()).isEqualTo(increasePoint - refundAmount);
+    }
+
+    @Test
+    @DisplayName("작품 구매 후 멤버의 포인트 내역")
+    void test4(){
+        // given
+        Member memberA = MEMBER_A.toMember(ENCODER);
+        final int increasePoint = 10000;
+        memberA.increasePoint(increasePoint);
+
+        // when
+        final int artPrice = 5000;
+        PointDetail pointDetail = PointDetail.insertPointDetail(memberA, PointType.PURCHASE, artPrice);
+
+        // then
+        assertThat(pointDetail.getPointType()).isEqualTo(PointType.PURCHASE);
+        assertThat(pointDetail.getAmount()).isEqualTo(artPrice);
+        assertThat(memberA.getAvailablePoint().getValue()).isEqualTo(increasePoint - artPrice);
+    }
+
+    @Test
+    @DisplayName("작품 판매 후 멤버의 포인트 내역")
+    void test5(){
+        // given
+        Member memberA = MEMBER_A.toMember(ENCODER);
+        final int increasePoint = 10000;
+        memberA.increasePoint(increasePoint);
+
+        // when
+        final int artPrice = 5000;
+        PointDetail pointDetail = PointDetail.insertPointDetail(memberA, PointType.SOLD, artPrice);
+
+        // then
+        assertThat(pointDetail.getPointType()).isEqualTo(PointType.SOLD);
+        assertThat(pointDetail.getAmount()).isEqualTo(artPrice);
+        assertThat(memberA.getAvailablePoint().getValue()).isEqualTo(increasePoint + artPrice);
+    }
+}
