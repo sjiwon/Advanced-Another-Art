04a571f255d0652e8b05d38fc45e9ea4587f034d
SeoJiWon <sjiwon4491@gmail.com>
2023-01-19 14:55:32 +0900

    init commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Advanced Another Art
+
+- [Origin Project](https://github.com/yumyeonghan/Another_Art)
+
+> 기존 프로젝트 도메인 재설계 및 리팩토링을 위한 레포지토리
+
+
+<br>
+
+## 주요 포인트
+- Vue.js 재구현
+- 백엔드 도메인 구조 다시 설계
+- 테스트 코드
+
---
+++ src/test/java/com/sjiwon/anotherart/AdvancedAnotherArtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sjiwon.anotherart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdvancedAnotherArtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
