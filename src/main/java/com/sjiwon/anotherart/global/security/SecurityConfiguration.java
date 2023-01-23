d91e1adeaa459f20bc3bb6901a3d5c7f5c95fca9
SeoJiWon <sjiwon4491@gmail.com>
2023-01-23 20:52:30 +0900

    feat: AuthenticationProvider 커스텀 적용

--- src/main/java/com/sjiwon/anotherart/global/security/SecurityConfiguration.java
+++ src/main/java/com/sjiwon/anotherart/global/security/SecurityConfiguration.java
@@ -1,13 +1,26 @@
 package com.sjiwon.anotherart.global.security;
 
+import com.sjiwon.anotherart.global.security.filter.AjaxAuthenticationFilter;
+import com.sjiwon.anotherart.global.security.handler.AjaxAuthenticationFailureHandler;
+import com.sjiwon.anotherart.global.security.provider.AjaxAuthenticationProvider;
+import com.sjiwon.anotherart.global.security.service.CustomUserDetailsService;
+import com.sjiwon.anotherart.member.domain.MemberRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -16,7 +29,11 @@ import java.util.List;
 
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
+@RequiredArgsConstructor
 public class SecurityConfiguration {
+    private final MemberRepository memberRepository;
+    private final AuthenticationConfiguration authenticationConfiguration;
+
     @Bean
     PasswordEncoder passwordEncoder() {
         return PasswordEncoderFactories.createDelegatingPasswordEncoder();
@@ -36,12 +53,44 @@ public class SecurityConfiguration {
     }
 
     @Bean
+    UserDetailsService userDetailsService() {
+        return new CustomUserDetailsService(memberRepository);
+    }
+
+    @Bean
+    AuthenticationProvider ajaxAuthenticationProvider() {
+        return new AjaxAuthenticationProvider(userDetailsService(), passwordEncoder());
+    }
+
+    @Bean
+    AuthenticationManager ajaxAuthenticationManager() throws Exception {
+        ProviderManager authenticationManager = (ProviderManager) authenticationConfiguration.getAuthenticationManager();
+        authenticationManager.getProviders().add(ajaxAuthenticationProvider());
+        return authenticationManager;
+    }
+
+    @Bean
+    AuthenticationFailureHandler ajaxAuthenticationFailureHandler() {
+        return new AjaxAuthenticationFailureHandler();
+    }
+
+    @Bean
+    AjaxAuthenticationFilter ajaxAuthenticationFilter() throws Exception {
+        AjaxAuthenticationFilter authenticationFilter = new AjaxAuthenticationFilter();
+        authenticationFilter.setAuthenticationManager(ajaxAuthenticationManager());
+        authenticationFilter.setAuthenticationFailureHandler(ajaxAuthenticationFailureHandler());
+        return authenticationFilter;
+    }
+
+    @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.csrf().disable();
         http.cors().configurationSource(corsConfigurationSource());
 
         http.formLogin().disable();
         http.httpBasic().disable();
+
+        http.addFilterAt(ajaxAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
 
         http.sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
