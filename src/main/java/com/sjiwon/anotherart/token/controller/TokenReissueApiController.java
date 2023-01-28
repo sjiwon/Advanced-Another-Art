b30c9b15741a821028456f8f1e08b1b8859215ba
SeoJiWon <sjiwon4491@gmail.com>
2023-01-28 21:41:37 +0900

    feat: 토큰 재발급 API [POST /api/token/reissue] 컨트롤러 엔드포인트 구현

---
+++ src/main/java/com/sjiwon/anotherart/token/controller/TokenReissueApiController.java
@@ -0,0 +1,27 @@
+package com.sjiwon.anotherart.token.controller;
+
+import com.sjiwon.anotherart.global.security.TokenResponse;
+import com.sjiwon.anotherart.token.service.TokenReissueService;
+import com.sjiwon.anotherart.token.utils.ExtractToken;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/token/reissue")
+@Api(tags = "토큰 재발급 API")
+public class TokenReissueApiController {
+    private final TokenReissueService tokenReissueService;
+
+    @PostMapping
+    @ApiOperation(value = "토큰 재발급 API", notes = "Authorization Header에 있는 Refresh Token으로 Access Token + Refresh Token 재발급")
+    public ResponseEntity<TokenResponse> reissueTokens(@ExtractToken String refreshToken) {
+        TokenResponse tokenResponse = tokenReissueService.reissueTokens(refreshToken);
+        return ResponseEntity.ok(tokenResponse);
+    }
+}
---
+++ src/test/java/com/sjiwon/anotherart/token/controller/TokenReissueApiControllerTest.java
@@ -0,0 +1,135 @@
+package com.sjiwon.anotherart.token.controller;
+
+import com.sjiwon.anotherart.common.ControllerTest;
+import com.sjiwon.anotherart.common.PasswordEncoderUtils;
+import com.sjiwon.anotherart.fixture.MemberFixture;
+import com.sjiwon.anotherart.global.security.exception.AuthErrorCode;
+import com.sjiwon.anotherart.member.domain.Member;
+import com.sjiwon.anotherart.member.domain.MemberRepository;
+import com.sjiwon.anotherart.token.service.RedisTokenService;
+import com.sjiwon.anotherart.token.utils.JwtTokenProvider;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.test.util.ReflectionTestUtils;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.springframework.http.HttpHeaders.AUTHORIZATION;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@DisplayName("Token [Controller Layer] -> TokenReissueApiController 테스트")
+class TokenReissueApiControllerTest extends ControllerTest {
+    @Autowired
+    MockMvc mockMvc;
+
+    @Autowired
+    JwtTokenProvider jwtTokenProvider;
+
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Autowired
+    RedisTokenService redisTokenService;
+
+    private static final String BASE_URL = "/api/token/reissue";
+    private static final String BEARER_TOKEN = "Bearer ";
+
+    @Nested
+    @DisplayName("토큰 재발급 테스트 [POST /api/token/reissue]")
+    class reissueToken {
+        @Test
+        @DisplayName("만료된 Refresh Token으로 인해 재발급에 실패한다")
+        void test1() throws Exception {
+            // given
+            ReflectionTestUtils.setField(jwtTokenProvider, "refreshTokenValidityInMilliseconds", 0L);
+            Member member = createMember();
+            String refreshToken = jwtTokenProvider.createRefreshToken(member.getId());
+
+            // when
+            MockHttpServletRequestBuilder requestBuilder = MockMvcRequestBuilders
+                    .post(BASE_URL)
+                    .header(AUTHORIZATION, BEARER_TOKEN + refreshToken);
+
+            // then
+            final AuthErrorCode expectedError = AuthErrorCode.INVALID_TOKEN;
+            HttpStatus expectedStatus = expectedError.getStatus();
+            String expectedMessage = expectedError.getMessage();
+
+            mockMvc.perform(requestBuilder)
+                    .andExpect(status().isForbidden())
+                    .andExpect(jsonPath("$.statusCode").exists())
+                    .andExpect(jsonPath("$.statusCode").value(expectedStatus.value()))
+                    .andExpect(jsonPath("$.errorCode").exists())
+                    .andExpect(jsonPath("$.errorCode").value(expectedStatus.getReasonPhrase()))
+                    .andExpect(jsonPath("$.message").exists())
+                    .andExpect(jsonPath("$.message").value(expectedMessage))
+                    .andDo(print());
+        }
+        
+        @Test
+        @DisplayName("RTR 정책에 의해서 이미 사용한 Refresh Token으로 Access Token + Refresh Token 재발급은 불가능하다")
+        void test2() throws Exception {
+            // given
+            ReflectionTestUtils.setField(jwtTokenProvider, "refreshTokenValidityInMilliseconds", 1209600L);
+            Member member = createMember();
+            String refreshToken = jwtTokenProvider.createRefreshToken(member.getId());
+            
+            // when
+            MockHttpServletRequestBuilder requestBuilder1 = MockMvcRequestBuilders
+                    .post(BASE_URL)
+                    .header(AUTHORIZATION, BEARER_TOKEN + refreshToken);
+            mockMvc.perform(requestBuilder1);
+
+            MockHttpServletRequestBuilder requestBuilder2 = MockMvcRequestBuilders
+                    .post(BASE_URL)
+                    .header(AUTHORIZATION, BEARER_TOKEN + refreshToken);
+
+            // then
+            final AuthErrorCode expectedError = AuthErrorCode.INVALID_TOKEN;
+            HttpStatus expectedStatus = expectedError.getStatus();
+            String expectedMessage = expectedError.getMessage();
+
+            mockMvc.perform(requestBuilder2)
+                    .andExpect(status().isForbidden())
+                    .andExpect(jsonPath("$.statusCode").exists())
+                    .andExpect(jsonPath("$.statusCode").value(expectedStatus.value()))
+                    .andExpect(jsonPath("$.errorCode").exists())
+                    .andExpect(jsonPath("$.errorCode").value(expectedStatus.getReasonPhrase()))
+                    .andExpect(jsonPath("$.message").exists())
+                    .andExpect(jsonPath("$.message").value(expectedMessage))
+                    .andDo(print());
+        }
+
+        @Test
+        @DisplayName("Refresh Token으로 Access Token + Refresh Token 재발급에 성공한다")
+        void test3() throws Exception {
+            // given
+            ReflectionTestUtils.setField(jwtTokenProvider, "refreshTokenValidityInMilliseconds", 1209600L);
+            Member member = createMember();
+            String refreshToken = jwtTokenProvider.createRefreshToken(member.getId());
+            redisTokenService.saveRefreshToken(refreshToken, member.getId());
+
+            // when
+            MockHttpServletRequestBuilder requestBuilder = MockMvcRequestBuilders
+                    .post(BASE_URL)
+                    .header(AUTHORIZATION, BEARER_TOKEN + refreshToken);
+
+            // then
+            mockMvc.perform(requestBuilder)
+                    .andExpect(status().isOk())
+                    .andExpect(jsonPath("$.accessToken").exists())
+                    .andExpect(jsonPath("$.refreshToken").exists())
+                    .andDo(print());
+        }
+    }
+
+    private Member createMember() {
+        return memberRepository.save(MemberFixture.A.toMember(PasswordEncoderUtils.getEncoder()));
+    }
+}
