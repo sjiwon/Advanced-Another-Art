532b9a79a62aed1551e38c8c01c02583427f1bdf
SeoJiWon <sjiwon4491@gmail.com>
2023-01-28 20:35:49 +0900

    feat: Authorization 헤더의 토큰을 추출하는 ArgumentResolver 구현

---
+++ src/main/java/com/sjiwon/anotherart/token/utils/ExtractTokenArgumentResolver.java
@@ -0,0 +1,22 @@
+package com.sjiwon.anotherart.token.utils;
+
+import org.springframework.core.MethodParameter;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+
+import javax.servlet.http.HttpServletRequest;
+
+public class ExtractTokenArgumentResolver implements HandlerMethodArgumentResolver {
+    @Override
+    public boolean supportsParameter(MethodParameter parameter) {
+        return parameter.hasParameterAnnotation(ExtractToken.class);
+    }
+
+    @Override
+    public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer, NativeWebRequest webRequest, WebDataBinderFactory binderFactory) {
+        HttpServletRequest request = webRequest.getNativeRequest(HttpServletRequest.class);
+        return AuthorizationExtractor.extractToken(request);
+    }
+}
