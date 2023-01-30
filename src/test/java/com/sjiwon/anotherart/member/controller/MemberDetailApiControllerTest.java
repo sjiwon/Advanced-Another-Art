1803b1520b98d20c800ebd64d853da58181a76a3
SeoJiWon <sjiwon4491@gmail.com>
2023-01-30 22:11:18 +0900

    test: POST /api/member/find/id 테스트 코드 수정

--- src/test/java/com/sjiwon/anotherart/member/controller/MemberDetailApiControllerTest.java
+++ src/test/java/com/sjiwon/anotherart/member/controller/MemberDetailApiControllerTest.java
@@ -1,8 +1,11 @@
 package com.sjiwon.anotherart.member.controller;
 
 import com.sjiwon.anotherart.common.ControllerTest;
+import com.sjiwon.anotherart.common.ObjectMapperUtils;
 import com.sjiwon.anotherart.common.PasswordEncoderUtils;
 import com.sjiwon.anotherart.fixture.MemberFixture;
+import com.sjiwon.anotherart.member.controller.dto.request.FindIdRequest;
+import com.sjiwon.anotherart.member.controller.utils.FindIdRequestUtils;
 import com.sjiwon.anotherart.member.domain.Member;
 import com.sjiwon.anotherart.member.domain.MemberRepository;
 import com.sjiwon.anotherart.member.exception.MemberErrorCode;
@@ -21,8 +24,7 @@ import static org.springframework.restdocs.headers.HeaderDocumentation.headerWit
 import static org.springframework.restdocs.headers.HeaderDocumentation.requestHeaders;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
-import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
-import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
+import static org.springframework.restdocs.payload.PayloadDocumentation.*;
 import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
 import static org.springframework.restdocs.request.RequestDocumentation.requestParameters;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -69,7 +71,7 @@ class MemberDetailApiControllerTest extends ControllerTest {
                     .andExpect(jsonPath("$.message").value(expectedError.getMessage()))
                     .andDo(
                             document(
-                                    "MemberApi/ChangeNickname/failure/case1",
+                                    "MemberApi/ChangeNickname/Failure/Case1",
                                     preprocessRequest(prettyPrint()),
                                     preprocessResponse(prettyPrint()),
                                     requestHeaders(
@@ -115,7 +117,7 @@ class MemberDetailApiControllerTest extends ControllerTest {
                     .andExpect(jsonPath("$.message").value(expectedError.getMessage()))
                     .andDo(
                             document(
-                                    "MemberApi/ChangeNickname/failure/case2",
+                                    "MemberApi/ChangeNickname/Failure/Case2",
                                     preprocessRequest(prettyPrint()),
                                     preprocessResponse(prettyPrint()),
                                     requestHeaders(
@@ -154,7 +156,7 @@ class MemberDetailApiControllerTest extends ControllerTest {
                     .andExpect(jsonPath("$").doesNotExist())
                     .andDo(
                             document(
-                                    "MemberApi/ChangeNickname/success",
+                                    "MemberApi/ChangeNickname/Success",
                                     preprocessRequest(prettyPrint()),
                                     preprocessResponse(prettyPrint()),
                                     requestHeaders(
@@ -169,21 +171,116 @@ class MemberDetailApiControllerTest extends ControllerTest {
     }
 
     @Nested
-    @DisplayName("사용자의 로그인 아이디를 찾는 테스트 [GET /api/member/find/id]")
+    @DisplayName("사용자의 로그인 아이디 조회 테스트 [POST /api/member/find/id]")
     class findLoginId {
         private static final String BASE_URL = "/api/member/find/id";
 
         @Test
-        @DisplayName("Access Token으로 사용자 아이디를 조회한다")
-        void test() throws Exception {
-            // given
+        @DisplayName("인증을 위한 이름이 사용자 정보와 다르면 예외가 발생한다")
+        void test1() throws Exception {
+            /// given
             Member member = createMemberA();
             String accessToken = jwtTokenProvider.createAccessToken(member.getId());
+            FindIdRequest request = FindIdRequestUtils.createRequest(member.getName() + "diff", member.getEmail().getValue());
 
             // when
             MockHttpServletRequestBuilder requestBuilder = MockMvcRequestBuilders
-                    .get(BASE_URL)
-                    .header(AUTHORIZATION, BEARER_TOKEN + accessToken);
+                    .post(BASE_URL)
+                    .header(AUTHORIZATION, BEARER_TOKEN + accessToken)
+                    .content(ObjectMapperUtils.objectToJson(request))
+                    .contentType(MediaType.APPLICATION_JSON);
+
+            // then
+            final MemberErrorCode expectedError = MemberErrorCode.INVALID_INFORMATION;
+            mockMvc.perform(requestBuilder)
+                    .andExpect(status().isBadRequest())
+                    .andExpect(jsonPath("$.statusCode").exists())
+                    .andExpect(jsonPath("$.statusCode").value(expectedError.getStatus().value()))
+                    .andExpect(jsonPath("$.errorCode").exists())
+                    .andExpect(jsonPath("$.errorCode").value(expectedError.getErrorCode()))
+                    .andExpect(jsonPath("$.message").exists())
+                    .andExpect(jsonPath("$.message").value(expectedError.getMessage()))
+                    .andDo(
+                            document(
+                                    "MemberApi/FindId/Failure/Case1",
+                                    preprocessRequest(prettyPrint()),
+                                    preprocessResponse(prettyPrint()),
+                                    requestHeaders(
+                                            headerWithName(AUTHORIZATION).description("Access Token")
+                                    ),
+                                    requestFields(
+                                            fieldWithPath("name").description("사용자 이름"),
+                                            fieldWithPath("email").description("사용자 이메일")
+                                    ),
+                                    responseFields(
+                                            fieldWithPath("statusCode").description("HTTP 상태 코드"),
+                                            fieldWithPath("errorCode").description("커스텀 예외 코드"),
+                                            fieldWithPath("message").description("예외 메시지")
+                                    )
+                            )
+                    );
+        }
+
+        @Test
+        @DisplayName("인증을 위한 이메일 사용자 정보와 다르면 예외가 발생한다")
+        void test2() throws Exception {
+            /// given
+            Member member = createMemberA();
+            String accessToken = jwtTokenProvider.createAccessToken(member.getId());
+            FindIdRequest request = FindIdRequestUtils.createRequest(member.getName(), "diff" + member.getEmail().getValue());
+
+            // when
+            MockHttpServletRequestBuilder requestBuilder = MockMvcRequestBuilders
+                    .post(BASE_URL)
+                    .header(AUTHORIZATION, BEARER_TOKEN + accessToken)
+                    .content(ObjectMapperUtils.objectToJson(request))
+                    .contentType(MediaType.APPLICATION_JSON);
+
+            // then
+            final MemberErrorCode expectedError = MemberErrorCode.INVALID_INFORMATION;
+            mockMvc.perform(requestBuilder)
+                    .andExpect(status().isBadRequest())
+                    .andExpect(jsonPath("$.statusCode").exists())
+                    .andExpect(jsonPath("$.statusCode").value(expectedError.getStatus().value()))
+                    .andExpect(jsonPath("$.errorCode").exists())
+                    .andExpect(jsonPath("$.errorCode").value(expectedError.getErrorCode()))
+                    .andExpect(jsonPath("$.message").exists())
+                    .andExpect(jsonPath("$.message").value(expectedError.getMessage()))
+                    .andDo(
+                            document(
+                                    "MemberApi/FindId/Failure/Case2",
+                                    preprocessRequest(prettyPrint()),
+                                    preprocessResponse(prettyPrint()),
+                                    requestHeaders(
+                                            headerWithName(AUTHORIZATION).description("Access Token")
+                                    ),
+                                    requestFields(
+                                            fieldWithPath("name").description("사용자 이름"),
+                                            fieldWithPath("email").description("사용자 이메일")
+                                    ),
+                                    responseFields(
+                                            fieldWithPath("statusCode").description("HTTP 상태 코드"),
+                                            fieldWithPath("errorCode").description("커스텀 예외 코드"),
+                                            fieldWithPath("message").description("예외 메시지")
+                                    )
+                            )
+                    );
+        }
+
+        @Test
+        @DisplayName("[Header: Access Token & Body: Name, Email]을 통해서 사용자 아이디를 조회한다")
+        void test3() throws Exception {
+            // given
+            Member member = createMemberA();
+            String accessToken = jwtTokenProvider.createAccessToken(member.getId());
+            FindIdRequest request = FindIdRequestUtils.createRequest(member.getName(), member.getEmail().getValue());
+
+            // when
+            MockHttpServletRequestBuilder requestBuilder = MockMvcRequestBuilders
+                    .post(BASE_URL)
+                    .header(AUTHORIZATION, BEARER_TOKEN + accessToken)
+                    .content(ObjectMapperUtils.objectToJson(request))
+                    .contentType(MediaType.APPLICATION_JSON);
 
             // then
             mockMvc.perform(requestBuilder)
@@ -191,11 +288,15 @@ class MemberDetailApiControllerTest extends ControllerTest {
                     .andExpect(jsonPath("$.value").value(member.getLoginId()))
                     .andDo(
                             document(
-                                    "MemberApi/FindId/success",
+                                    "MemberApi/FindId/Success",
                                     preprocessRequest(prettyPrint()),
                                     preprocessResponse(prettyPrint()),
                                     requestHeaders(
                                             headerWithName(AUTHORIZATION).description("Access Token")
+                                    ),
+                                    requestFields(
+                                            fieldWithPath("name").description("사용자 이름"),
+                                            fieldWithPath("email").description("사용자 이메일")
                                     ),
                                     responseFields(
                                             fieldWithPath("value").description("사용자 로그인 아이디")
