88b6e412bf1cf4fa1469d0171dfc018bdb60e544
SeoJiWon <sjiwon4491@gmail.com>
2023-01-30 21:19:23 +0900

    feat: 사용자 로그인 아이디 찾기 API [GET /api/member/find/id] 엔드포인트 구현

---
+++ src/main/java/com/sjiwon/anotherart/global/dto/SimpleWrapper.java
@@ -0,0 +1,13 @@
+package com.sjiwon.anotherart.global.dto;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class SimpleWrapper<T> {
+    private T value;
+}
--- src/main/java/com/sjiwon/anotherart/member/controller/MemberDetailApiController.java
+++ src/main/java/com/sjiwon/anotherart/member/controller/MemberDetailApiController.java
@@ -1,5 +1,6 @@
 package com.sjiwon.anotherart.member.controller;
 
+import com.sjiwon.anotherart.global.dto.SimpleWrapper;
 import com.sjiwon.anotherart.member.service.MemberService;
 import com.sjiwon.anotherart.token.utils.ExtractPayload;
 import io.swagger.annotations.Api;
@@ -7,10 +8,7 @@ import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -25,5 +23,12 @@ public class MemberDetailApiController {
     public ResponseEntity<Void> changeNickname(@ExtractPayload Long memberId, @RequestParam String changeNickname) {
         memberService.changeNickname(memberId, changeNickname);
         return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping("/find/id")
+    @ApiOperation(value = "아이디 찾기 API", notes = "Access Token의 Payload를 통해서 memberId에 해당하는 사용자의 아이디 찾기")
+    public ResponseEntity<SimpleWrapper<String>> findId(@ExtractPayload Long memberId) {
+        String loginId = memberService.findLoginIdViaMemberId(memberId);
+        return ResponseEntity.ok(new SimpleWrapper<>(loginId));
     }
 }
