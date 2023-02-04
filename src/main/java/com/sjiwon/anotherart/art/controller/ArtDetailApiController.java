b25850c08cc356d10966184b12dc009ea0918f1f
SeoJiWon <sjiwon4491@gmail.com>
2023-02-04 19:52:54 +0900

    feat: 작품 설명 수정 API [PATCH /api/art/{artId}/description] 엔드포인트 구현

--- src/main/java/com/sjiwon/anotherart/art/controller/ArtDetailApiController.java
+++ src/main/java/com/sjiwon/anotherart/art/controller/ArtDetailApiController.java
@@ -1,17 +1,12 @@
 package com.sjiwon.anotherart.art.controller;
 
 import com.sjiwon.anotherart.art.service.ArtService;
-import com.sjiwon.anotherart.global.annotation.ExtractPayload;
-import com.sjiwon.anotherart.global.annotation.RequiredToken;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import static org.springframework.http.MediaType.APPLICATION_FORM_URLENCODED_VALUE;
 
@@ -22,12 +17,19 @@ import static org.springframework.http.MediaType.APPLICATION_FORM_URLENCODED_VAL
 public class ArtDetailApiController {
     private final ArtService artService;
 
-    @RequiredToken
     @PreAuthorize("hasRole('USER')")
     @PostMapping(value = "/duplicate-check", consumes = APPLICATION_FORM_URLENCODED_VALUE)
     @ApiOperation(value = "작품명 중복 체크 API", notes = "작품 등록 과정에서 작품명 중복체크를 진행하는 API")
-    public ResponseEntity<Void> artNameDuplicateCheck(@ExtractPayload Long memberId, @RequestParam String artName) {
+    public ResponseEntity<Void> artNameDuplicateCheck(@RequestParam String artName) {
         artService.artNameDuplicateCheck(artName);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PreAuthorize("hasRole('USER')")
+    @PatchMapping(value = "/{artId}/description", consumes = APPLICATION_FORM_URLENCODED_VALUE)
+    @ApiOperation(value = "작품 설명 수정 API", notes = "작품 설명을 수정하는 API")
+    public ResponseEntity<Void> changeDescription(@PathVariable Long artId, @RequestParam String changeDescription) {
+        artService.changeDescription(artId, changeDescription);
         return ResponseEntity.noContent().build();
     }
 }
