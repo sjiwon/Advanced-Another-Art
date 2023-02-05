83662749b51bef32f6cc666224ca9369de7f374d
SeoJiWon <sjiwon4491@gmail.com>
2023-02-05 20:41:49 +0900

    feat: 작품 좋아요 취소 API [DELETE /api/art/{artId}/like] 엔드포인트 구현

--- src/main/java/com/sjiwon/anotherart/favorite/controller/FavoriteApiController.java
+++ src/main/java/com/sjiwon/anotherart/favorite/controller/FavoriteApiController.java
@@ -7,10 +7,7 @@ import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -22,8 +19,16 @@ public class FavoriteApiController {
     @PreAuthorize("hasRole('USER')")
     @PostMapping
     @ApiOperation(value = "작품 좋아요 등록 API", notes = "사용자가 해당 작품에 대한 좋아요를 등록하기 위한 API")
-    public ResponseEntity<Void> likeMarking(@PathVariable Long artId, @ExtractPayload Long memberId) {
-        favoriteService.likeMarking(artId, memberId);
+    public ResponseEntity<Void> like(@PathVariable Long artId, @ExtractPayload Long memberId) {
+        favoriteService.like(artId, memberId);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PreAuthorize("hasRole('USER')")
+    @DeleteMapping
+    @ApiOperation(value = "작품 좋아요 취소 API", notes = "좋아요 등록을 한 작품에 대해서 좋아요 취소를 하기 위한 API")
+    public ResponseEntity<Void> likeCancel(@PathVariable Long artId, @ExtractPayload Long memberId) {
+        favoriteService.likeCancel(artId, memberId);
         return ResponseEntity.noContent().build();
     }
 }
