5c22315eb67be5dc214614172c0a0ae3c6308cef
SeoJiWon <sjiwon4491@gmail.com>
2023-02-05 17:55:45 +0900

    test: 작품 좋아요 등록 API 테스트 코드

---
+++ src/test/java/com/sjiwon/anotherart/favorite/controller/FavoriteApiControllerTest.java
@@ -0,0 +1,228 @@
+package com.sjiwon.anotherart.favorite.controller;
+
+import com.sjiwon.anotherart.art.domain.Art;
+import com.sjiwon.anotherart.art.domain.ArtRepository;
+import com.sjiwon.anotherart.common.ControllerTest;
+import com.sjiwon.anotherart.favorite.domain.Favorite;
+import com.sjiwon.anotherart.favorite.domain.FavoriteRepository;
+import com.sjiwon.anotherart.favorite.exception.FavoriteErrorCode;
+import com.sjiwon.anotherart.fixture.ArtFixture;
+import com.sjiwon.anotherart.fixture.MemberFixture;
+import com.sjiwon.anotherart.global.security.exception.AuthErrorCode;
+import com.sjiwon.anotherart.member.domain.Member;
+import com.sjiwon.anotherart.member.domain.MemberRepository;
+import com.sjiwon.anotherart.token.utils.JwtTokenProvider;
+import lombok.RequiredArgsConstructor;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+
+import static org.springframework.http.HttpHeaders.AUTHORIZATION;
+import static org.springframework.restdocs.headers.HeaderDocumentation.headerWithName;
+import static org.springframework.restdocs.headers.HeaderDocumentation.requestHeaders;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
+import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
+import static org.springframework.restdocs.request.RequestDocumentation.pathParameters;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@DisplayName("Favorite [Controller Layer] -> FavoriteApiController 테스트")
+@RequiredArgsConstructor
+class FavoriteApiControllerTest extends ControllerTest {
+    private final MockMvc mockMvc;
+    private final MemberRepository memberRepository;
+    private final JwtTokenProvider jwtTokenProvider;
+    private final ArtRepository artRepository;
+    private final FavoriteRepository favoriteRepository;
+
+    private static final String BEARER_TOKEN = "Bearer ";
+
+    @Nested
+    @DisplayName("작품 좋아요 등록 테스트 [POST /api/art/{artId}/like]")
+    class likeMarking {
+        private static final String BASE_URL = "/api/art/{artId}/like";
+
+        @Test
+        @DisplayName("Authorization 헤더에 Access Token이 없음에 따라 예외가 발생한다")
+        void test1() throws Exception {
+            // given
+            Member owner = createMemberA();
+            Art art = createArt(owner);
+
+            // when
+            MockHttpServletRequestBuilder requestBuilder = RestDocumentationRequestBuilders
+                    .post(BASE_URL, art.getId());
+
+            // then
+            final AuthErrorCode expectedError = AuthErrorCode.INVALID_TOKEN;
+            mockMvc.perform(requestBuilder)
+                    .andExpect(status().isForbidden())
+                    .andExpect(jsonPath("$.statusCode").exists())
+                    .andExpect(jsonPath("$.statusCode").value(expectedError.getStatus().value()))
+                    .andExpect(jsonPath("$.errorCode").exists())
+                    .andExpect(jsonPath("$.errorCode").value(expectedError.getErrorCode()))
+                    .andExpect(jsonPath("$.message").exists())
+                    .andExpect(jsonPath("$.message").value(expectedError.getMessage()))
+                    .andDo(
+                            document(
+                                    "FavoriteApi/LikeMarking/Failure/Case1",
+                                    preprocessRequest(prettyPrint()),
+                                    preprocessResponse(prettyPrint()),
+                                    pathParameters(
+                                            parameterWithName("artId").description("좋아요 등록을 할 작품 ID(PK)")
+                                    ),
+                                    responseFields(
+                                            fieldWithPath("statusCode").description("HTTP 상태 코드"),
+                                            fieldWithPath("errorCode").description("커스텀 예외 코드"),
+                                            fieldWithPath("message").description("예외 메시지")
+                                    )
+                            )
+                    );
+        }
+
+        @Test
+        @DisplayName("작품 소유자는 본인의 작품을 좋아요 등록할 수 없고 그에 따라서 예외가 발생한다")
+        void test2() throws Exception {
+            // given
+            Member owner = createMemberA();
+            String accessToken = jwtTokenProvider.createAccessToken(owner.getId());
+            Art art = createArt(owner);
+
+            // when
+            MockHttpServletRequestBuilder requestBuilder = RestDocumentationRequestBuilders
+                    .post(BASE_URL, art.getId())
+                    .header(AUTHORIZATION, BEARER_TOKEN + accessToken);
+
+            // then
+            final FavoriteErrorCode expectedError = FavoriteErrorCode.INVALID_LIKE_MARKING_BY_ART_OWNER;
+            mockMvc.perform(requestBuilder)
+                    .andExpect(status().isBadRequest())
+                    .andExpect(jsonPath("$.statusCode").exists())
+                    .andExpect(jsonPath("$.statusCode").value(expectedError.getStatus().value()))
+                    .andExpect(jsonPath("$.errorCode").exists())
+                    .andExpect(jsonPath("$.errorCode").value(expectedError.getErrorCode()))
+                    .andExpect(jsonPath("$.message").exists())
+                    .andExpect(jsonPath("$.message").value(expectedError.getMessage()))
+                    .andDo(
+                            document(
+                                    "FavoriteApi/LikeMarking/Failure/Case2",
+                                    preprocessRequest(prettyPrint()),
+                                    preprocessResponse(prettyPrint()),
+                                    requestHeaders(
+                                            headerWithName(AUTHORIZATION).description("Access Token")
+                                    ),
+                                    pathParameters(
+                                            parameterWithName("artId").description("좋아요 등록을 할 작품 ID(PK)")
+                                    ),
+                                    responseFields(
+                                            fieldWithPath("statusCode").description("HTTP 상태 코드"),
+                                            fieldWithPath("errorCode").description("커스텀 예외 코드"),
+                                            fieldWithPath("message").description("예외 메시지")
+                                    )
+                            )
+                    );
+        }
+
+        @Test
+        @DisplayName("이미 좋아요 등록을 한 후 또 다시 좋아요 등록 요청을 하게 되면 예외가 발생한다")
+        void test3() throws Exception {
+            // given
+            Member owner = createMemberA();
+            Art art = createArt(owner);
+
+            Member member = createMemberB();
+            String accessToken = jwtTokenProvider.createAccessToken(member.getId());
+            advanceLikeMarking(art, member);
+
+            // when
+            MockHttpServletRequestBuilder requestBuilder = RestDocumentationRequestBuilders
+                    .post(BASE_URL, art.getId())
+                    .header(AUTHORIZATION, BEARER_TOKEN + accessToken);
+
+            // then
+            final FavoriteErrorCode expectedError = FavoriteErrorCode.ALREADY_LIKE_MARKING;
+            mockMvc.perform(requestBuilder)
+                    .andExpect(status().isBadRequest())
+                    .andExpect(jsonPath("$.statusCode").exists())
+                    .andExpect(jsonPath("$.statusCode").value(expectedError.getStatus().value()))
+                    .andExpect(jsonPath("$.errorCode").exists())
+                    .andExpect(jsonPath("$.errorCode").value(expectedError.getErrorCode()))
+                    .andExpect(jsonPath("$.message").exists())
+                    .andExpect(jsonPath("$.message").value(expectedError.getMessage()))
+                    .andDo(
+                            document(
+                                    "FavoriteApi/LikeMarking/Failure/Case3",
+                                    preprocessRequest(prettyPrint()),
+                                    preprocessResponse(prettyPrint()),
+                                    requestHeaders(
+                                            headerWithName(AUTHORIZATION).description("Access Token")
+                                    ),
+                                    pathParameters(
+                                            parameterWithName("artId").description("좋아요 등록을 할 작품 ID(PK)")
+                                    ),
+                                    responseFields(
+                                            fieldWithPath("statusCode").description("HTTP 상태 코드"),
+                                            fieldWithPath("errorCode").description("커스텀 예외 코드"),
+                                            fieldWithPath("message").description("예외 메시지")
+                                    )
+                            )
+                    );
+        }
+        
+        @Test
+        @DisplayName("작품에 대한 좋아요 등록을 성공한다")
+        void test4() throws Exception {
+            // given
+            Member owner = createMemberA();
+            Art art = createArt(owner);
+
+            Member member = createMemberB();
+            String accessToken = jwtTokenProvider.createAccessToken(member.getId());
+
+            // when
+            MockHttpServletRequestBuilder requestBuilder = RestDocumentationRequestBuilders
+                    .post(BASE_URL, art.getId())
+                    .header(AUTHORIZATION, BEARER_TOKEN + accessToken);
+
+            // then
+            mockMvc.perform(requestBuilder)
+                    .andExpect(status().isNoContent())
+                    .andExpect(jsonPath("$").doesNotExist())
+                    .andDo(
+                            document(
+                                    "FavoriteApi/LikeMarking/Success",
+                                    preprocessRequest(prettyPrint()),
+                                    preprocessResponse(prettyPrint()),
+                                    requestHeaders(
+                                            headerWithName(AUTHORIZATION).description("Access Token")
+                                    ),
+                                    pathParameters(
+                                            parameterWithName("artId").description("좋아요 등록을 할 작품 ID(PK)")
+                                    )
+                            )
+                    );
+        }
+    }
+
+    private void advanceLikeMarking(Art art, Member member) {
+        favoriteRepository.save(Favorite.favoriteMarking(art.getId(), member.getId()));
+    }
+
+    private Member createMemberA() {
+        return memberRepository.save(MemberFixture.A.toMember());
+    }
+
+    private Member createMemberB() {
+        return memberRepository.save(MemberFixture.B.toMember());
+    }
+
+    private Art createArt(Member owner) {
+        return artRepository.save(ArtFixture.B.toArt(owner));
+    }
+}
