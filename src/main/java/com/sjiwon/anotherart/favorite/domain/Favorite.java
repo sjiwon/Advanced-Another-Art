dbe6b69454351cc6b33ac825e025bfef2873343f
SeoJiWon <sjiwon4491@gmail.com>
2023-02-05 17:13:09 +0900

    refactor: Favorite - Art/Member 직접 참조로 수정

--- src/main/java/com/sjiwon/anotherart/favorite/domain/Favorite.java
+++ src/main/java/com/sjiwon/anotherart/favorite/domain/Favorite.java
@@ -1,5 +1,7 @@
 package com.sjiwon.anotherart.favorite.domain;
 
+import com.sjiwon.anotherart.art.domain.Art;
+import com.sjiwon.anotherart.member.domain.Member;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
@@ -16,19 +18,21 @@ public class Favorite {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name = "art_id", nullable = false)
-    private Long artId;
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "art_id", referencedColumnName = "id", nullable = false, updatable = false)
+    private Art art;
 
-    @Column(name = "user_id", nullable = false)
-    private Long userId;
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "user_id", referencedColumnName = "id", nullable = false, updatable = false)
+    private Member member;
 
     @Builder
-    private Favorite(Long artId, Long userId) {
-        this.artId = artId;
-        this.userId = userId;
+    private Favorite(Art art, Member member) {
+        this.art = art;
+        this.member = member;
     }
 
-    public static Favorite favoriteMarking(Long artId, Long userId) {
-        return new Favorite(artId, userId);
+    public static Favorite favoriteMarking(Art art, Member member) {
+        return new Favorite(art, member);
     }
 }
