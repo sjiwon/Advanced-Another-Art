b4c8c67e931869ea9d49dc6c0be619de5eb264c5
SeoJiWon <sjiwon4491@gmail.com>
2023-02-09 20:30:48 +0900

    refactor: 파일 업로드 관련 유틸 클래스 분리

--- src/main/java/com/sjiwon/anotherart/art/service/ArtService.java
+++ src/main/java/com/sjiwon/anotherart/art/service/ArtService.java
@@ -3,25 +3,20 @@ package com.sjiwon.anotherart.art.service;
 import com.sjiwon.anotherart.art.domain.Art;
 import com.sjiwon.anotherart.art.domain.ArtRepository;
 import com.sjiwon.anotherart.art.domain.UploadImage;
-import com.sjiwon.anotherart.art.domain.hashtag.HashtagRepository;
 import com.sjiwon.anotherart.art.exception.ArtErrorCode;
 import com.sjiwon.anotherart.art.service.dto.request.ArtRegisterRequestDto;
+import com.sjiwon.anotherart.art.utils.FileUploadUtils;
 import com.sjiwon.anotherart.auction.domain.Auction;
 import com.sjiwon.anotherart.auction.domain.AuctionRepository;
 import com.sjiwon.anotherart.auction.domain.Period;
 import com.sjiwon.anotherart.auction.domain.record.AuctionRecordRepository;
 import com.sjiwon.anotherart.global.exception.AnotherArtException;
-import com.sjiwon.anotherart.global.exception.GlobalErrorCode;
 import com.sjiwon.anotherart.member.domain.Member;
 import com.sjiwon.anotherart.member.service.MemberFindService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.multipart.MultipartFile;
 
-import java.io.File;
-import java.io.IOException;
 import java.util.HashSet;
 import java.util.List;
 
@@ -31,13 +26,10 @@ import java.util.List;
 public class ArtService {
     private final ArtRepository artRepository;
     private final ArtFindService artFindService;
-    private final HashtagRepository hashtagRepository;
     private final MemberFindService memberFindService;
     private final AuctionRepository auctionRepository;
     private final AuctionRecordRepository auctionRecordRepository;
-
-    @Value("${file.dir}")
-    private String fileDir;
+    private final FileUploadUtils fileUploadUtils;
 
     @Transactional
     public void artRegistration(Long ownerId, ArtRegisterRequestDto request) {
@@ -56,16 +48,8 @@ public class ArtService {
                 .uploadImage(UploadImage.from(request.getFile()))
                 .hashtags(new HashSet<>(request.getHashtagList()))
                 .build();
-        processFileUpload(request.getFile(), art.getStorageName());
+        fileUploadUtils.proceedingFileUpload(request.getFile(), art.getStorageName());
         return artRepository.save(art);
-    }
-
-    private void processFileUpload(MultipartFile file, String storageName) {
-        try {
-            file.transferTo(new File(fileDir + storageName));
-        } catch (IOException e) {
-            throw AnotherArtException.type(GlobalErrorCode.INTERNAL_SERVER_ERROR);
-        }
     }
 
     private void validateArtTypeAndExecuteAuctionLogic(Art art, Period period) {
