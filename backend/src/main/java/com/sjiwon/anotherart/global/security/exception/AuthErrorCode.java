aab87f00223ca8a6e4c76ec83cf0d214984828fa
SeoJiWon <sjiwon4491@gmail.com>
2023-05-03 17:16:29 +0900

    feat: Security 인증/인가 관련 Exception

---
+++ backend/src/main/java/com/sjiwon/anotherart/global/security/exception/AuthErrorCode.java
@@ -0,0 +1,20 @@
+package com.sjiwon.anotherart.global.security.exception;
+
+import com.sjiwon.anotherart.global.exception.ErrorCode;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@RequiredArgsConstructor
+public enum AuthErrorCode implements ErrorCode {
+    INVALID_AUTH_CONTENT_TYPE(HttpStatus.BAD_REQUEST, "AUTH_001", "인증방식이 올바르지 않습니다."),
+    INVALID_AUTH_DATA(HttpStatus.BAD_REQUEST, "AUTH_002", "아이디나 비밀번호를 정확하게 입력해주세요."),
+    INVALID_TOKEN(HttpStatus.FORBIDDEN, "AUTH_003", "권한이 없습니다."),
+    LOGIN_REQUIRED(HttpStatus.UNAUTHORIZED, "AUTH_004", "로그인이 필요합니다."),
+    ;
+
+    private final HttpStatus status;
+    private final String errorCode;
+    private final String message;
+}
