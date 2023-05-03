6fedd0bc2ed2159e5912c64d87246e010a92a04e
SeoJiWon <sjiwon4491@gmail.com>
2023-05-03 16:01:03 +0900

    init: backend re-init

---
+++ backend/src/main/java/com/sjiwon/anotherart/AnotherArtApplication.java
@@ -0,0 +1,11 @@
+package com.sjiwon.anotherart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnotherArtApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(AnotherArtApplication.class, args);
+	}
+}
