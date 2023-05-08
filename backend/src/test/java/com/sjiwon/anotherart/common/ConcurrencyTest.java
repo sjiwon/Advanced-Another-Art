28788a5f895b5b3123c29667cb925dfecfdad096
SeoJiWon <sjiwon4491@gmail.com>
2023-05-08 18:30:51 +0900

    feat: 일반 작품 구매에 대한 동시성 테스트

--- backend/src/test/java/com/sjiwon/anotherart/common/ConcurrencyTest.java
+++ backend/src/test/java/com/sjiwon/anotherart/common/ConcurrencyTest.java
@@ -2,14 +2,21 @@ package com.sjiwon.anotherart.common;
 
 import com.sjiwon.anotherart.art.domain.ArtRepository;
 import com.sjiwon.anotherart.auction.domain.AuctionRepository;
-import com.sjiwon.anotherart.member.domain.MemberRepository;
+import com.sjiwon.anotherart.common.utils.PasswordEncoderUtils;
+import com.sjiwon.anotherart.member.domain.*;
+import com.sjiwon.anotherart.member.domain.point.PointRecord;
 import com.sjiwon.anotherart.member.domain.point.PointRecordRepository;
 import org.junit.jupiter.api.AfterEach;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.util.ReflectionTestUtils;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.sjiwon.anotherart.member.domain.point.PointType.CHARGE;
 
 @SpringBootTest
 public class ConcurrencyTest {
@@ -31,8 +38,53 @@ public class ConcurrencyTest {
     @Autowired
     protected AuctionRepository auctionRepository;
 
+    protected final List<Long> memberIds = new ArrayList<>();
+
+    protected void createMembers(int threadCount) {
+        List<Member> members = new ArrayList<>();
+        List<PointRecord> pointRecords = new ArrayList<>();
+
+        for (int i = 1; i <= threadCount; i++) {
+            Member member = createMember(i);
+            increaseMemberPoint(member);
+            members.add(member);
+            pointRecords.add(PointRecord.addPointRecord(member, CHARGE, 100_000_000));
+        }
+
+        memberRepository.saveAll(members);
+        pointRecordRepository.saveAll(pointRecords);
+        members.forEach(member -> memberIds.add(member.getId()));
+    }
+
+    private Member createMember(int index) {
+        return Member.createMember(
+                "name" + index,
+                Nickname.from("nick" + index),
+                "loginid" + index,
+                Password.encrypt("abcABC123!@#", PasswordEncoderUtils.getEncoder()),
+                "경기대학교",
+                generateRandomPhoneNumber(),
+                Email.from("test" + index + "@gmail.com"),
+                Address.of(12345, "기본 주소", "상세 주소")
+        );
+    }
+
+    private String generateRandomPhoneNumber() {
+        String first = "010";
+        String second = String.valueOf((int) (Math.random() * 9000 + 1000));
+        String third = String.valueOf((int) (Math.random() * 9000 + 1000));
+
+        return first + second + third;
+    }
+
+    private void increaseMemberPoint(Member member) {
+        ReflectionTestUtils.setField(member.getPoint(), "totalPoint", 100_000_000);
+        ReflectionTestUtils.setField(member.getPoint(), "availablePoint", 100_000_000);
+    }
+
     @AfterEach
     void clearDatabase() {
         databaseCleaner.cleanUpDatabase();
+        memberIds.clear();
     }
 }
--- backend/src/test/java/com/sjiwon/anotherart/purchase/service/PurchaseServieConcurrencyTest.java
+++ backend/src/test/java/com/sjiwon/anotherart/purchase/service/PurchaseServieConcurrencyTest.java
@@ -1,7 +1,6 @@
-package com.sjiwon.anotherart.auction.service;
+package com.sjiwon.anotherart.purchase.service;
 
 import com.sjiwon.anotherart.art.domain.Art;
-import com.sjiwon.anotherart.auction.domain.Auction;
 import com.sjiwon.anotherart.common.ConcurrencyTest;
 import com.sjiwon.anotherart.member.domain.Member;
 import lombok.extern.slf4j.Slf4j;
@@ -14,42 +13,39 @@ import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
-import static com.sjiwon.anotherart.fixture.ArtFixture.AUCTION_1;
-import static com.sjiwon.anotherart.fixture.AuctionFixture.AUCTION_OPEN_NOW;
+import static com.sjiwon.anotherart.fixture.ArtFixture.GENERAL_1;
 import static com.sjiwon.anotherart.fixture.MemberFixture.MEMBER_A;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @Slf4j
-@DisplayName("Auction [Service Layer] -> BidService 동시성 테스트")
-class BidServiceConcurrencyTest extends ConcurrencyTest {
+@DisplayName("Purchase [Service Layer] -> PurchaseServieConcurrency 동시성 테스트")
+class PurchaseServieConcurrencyTest extends ConcurrencyTest {
     @Autowired
-    private BidService bidService;
+    private PurchaseService purchaseService;
 
     private static final int THREAD_COUNT = 10;
-    private Auction auction;
+    private Art art;
 
     @BeforeEach
     void setUp() {
         Member owner = memberRepository.save(MEMBER_A.toMember());
-        Art art = artRepository.save(AUCTION_1.toArt(owner));
-        auction = auctionRepository.save(AUCTION_OPEN_NOW.toAuction(art));
+        art = artRepository.save(GENERAL_1.toArt(owner));
 
-        createMembers(THREAD_COUNT); // 10명의 입찰자 생성
+        createMembers(THREAD_COUNT); // 10명의 구매자 생성
     }
 
     @Test
-    @DisplayName("10명의 입찰자가 동시에 동일한 가격으로 입찰을 진행한다면 1명만 입찰에 성공해야 한다")
-    void concurrencyBid() throws InterruptedException {
+    @DisplayName("10명의 구매자가 동시에 일반 작품 구매를 진행한다면 1명만 구매에 성공해야 한다")
+    void concurrencyPurchase() throws InterruptedException {
         // given
         ExecutorService executorService = Executors.newFixedThreadPool(THREAD_COUNT);
         CountDownLatch countDownLatch = new CountDownLatch(THREAD_COUNT);
 
         // when
-        final int bidAmount = auction.getHighestBidPrice() + 50_000;
-        for (Long bidderId : memberIds) {
+        for (Long buyerId : memberIds) {
             executorService.submit(() -> {
                 try {
-                    bidService.bid(auction.getId(), bidderId, bidAmount);
+                    purchaseService.purchaseArt(art.getId(), buyerId);
                 } catch (Exception e) {
                     log.error(e.getMessage());
                 } finally {
@@ -60,17 +56,15 @@ class BidServiceConcurrencyTest extends ConcurrencyTest {
         countDownLatch.await();
 
         // then
-        assertThat(getAuctionRecordCount()).isEqualTo(1);
+        assertThat(getPurchaseRecordCount()).isEqualTo(1);
     }
 
-    private Long getAuctionRecordCount() {
+    private Long getPurchaseRecordCount() {
         return em.createQuery(
-                        "SELECT COUNT(ac)" +
-                                " FROM AuctionRecord ac" +
-                                " WHERE ac.auction.id = :auctionId",
+                        "SELECT COUNT(p)" +
+                                " FROM Purchase p",
                         Long.class
                 )
-                .setParameter("auctionId", auction.getId())
                 .getResultList()
                 .get(0);
     }
