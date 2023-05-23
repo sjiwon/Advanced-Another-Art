d936262c522d20c192adef9014ff4b0ae152bbfe
SeoJiWon <sjiwon4491@gmail.com>
2023-05-23 09:12:37 +0900

    refactor: TestContainers AfterAllCallback 추가

--- backend/src/test/java/com/sjiwon/anotherart/common/config/TestContainerConfiguration.java
+++ backend/src/test/java/com/sjiwon/anotherart/common/config/TestContainerConfiguration.java
@@ -1,24 +1,29 @@
 package com.sjiwon.anotherart.common.config;
 
+import org.junit.jupiter.api.extension.AfterAllCallback;
 import org.junit.jupiter.api.extension.BeforeAllCallback;
 import org.junit.jupiter.api.extension.ExtensionContext;
 import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.utility.DockerImageName;
 
-public class TestContainerConfiguration implements BeforeAllCallback {
+public class TestContainerConfiguration implements BeforeAllCallback, AfterAllCallback {
     private static final String REDIS_IMAGE = "redis:7.0.8-alpine";
     private static final int REDIS_PORT = 6379;
+    private static final GenericContainer<?> REDIS_CONTAINER =
+            new GenericContainer<>(DockerImageName.parse(REDIS_IMAGE))
+                    .withExposedPorts(REDIS_PORT)
+                    .withReuse(true);
 
     @Override
     public void beforeAll(ExtensionContext context) {
-        GenericContainer<?> REDIS_CONTAINER =
-                new GenericContainer<>(DockerImageName.parse(REDIS_IMAGE))
-                        .withExposedPorts(REDIS_PORT)
-                        .withReuse(true);
-
         REDIS_CONTAINER.start();
 
         System.setProperty("spring.redis.host", REDIS_CONTAINER.getHost());
         System.setProperty("spring.redis.port", REDIS_CONTAINER.getMappedPort(REDIS_PORT).toString());
+    }
+
+    @Override
+    public void afterAll(ExtensionContext context) {
+        REDIS_CONTAINER.stop();
     }
 }
