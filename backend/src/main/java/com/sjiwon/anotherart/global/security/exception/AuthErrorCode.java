2800f27901e91b5366bdd30025b941bcac7f8f2f
SeoJiWon <sjiwon4491@gmail.com>
2023-11-05 20:14:59 +0900

    refactor: AuthErrorCode HttpStatus 수정

--- backend/src/main/java/com/sjiwon/anotherart/global/security/exception/AuthErrorCode.java
+++ backend/src/main/java/com/sjiwon/anotherart/global/security/exception/AuthErrorCode.java
@@ -5,13 +5,17 @@ import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
+import static org.springframework.http.HttpStatus.FORBIDDEN;
+import static org.springframework.http.HttpStatus.UNAUTHORIZED;
+
 @Getter
 @RequiredArgsConstructor
 public enum AuthErrorCode implements ErrorCode {
-    INVALID_AUTH_CONTENT_TYPE(HttpStatus.BAD_REQUEST, "AUTH_001", "인증방식이 올바르지 않습니다."),
-    INVALID_AUTH_DATA(HttpStatus.BAD_REQUEST, "AUTH_002", "아이디나 비밀번호를 정확하게 입력해주세요."),
-    INVALID_PERMISSION(HttpStatus.FORBIDDEN, "AUTH_003", "권한이 없습니다."),
-    LOGIN_REQUIRED(HttpStatus.UNAUTHORIZED, "AUTH_004", "로그인이 필요합니다."),
+    INVALID_AUTH_CONTENT_TYPE(BAD_REQUEST, "AUTH_001", "인증방식이 올바르지 않습니다."),
+    INVALID_AUTH_DATA(UNAUTHORIZED, "AUTH_002", "아이디나 비밀번호를 정확하게 입력해주세요."),
+    INVALID_PERMISSION(FORBIDDEN, "AUTH_003", "권한이 없습니다."),
+    LOGIN_REQUIRED(FORBIDDEN, "AUTH_004", "로그인이 필요합니다."),
     ;
 
     private final HttpStatus status;
