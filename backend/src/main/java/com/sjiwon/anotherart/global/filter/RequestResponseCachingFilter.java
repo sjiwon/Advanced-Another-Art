7786f1e273e486f5e40f41ea8ece49eb8249218a
SeoJiWon <sjiwon4491@gmail.com>
2023-11-05 15:23:51 +0900

    feat: request/response 캐싱 필터 적용

---
+++ backend/src/main/java/com/sjiwon/anotherart/global/filter/RequestResponseCachingFilter.java
@@ -0,0 +1,26 @@
+package com.sjiwon.anotherart.global.filter;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.web.util.ContentCachingResponseWrapper;
+
+import java.io.IOException;
+
+@Component
+public class RequestResponseCachingFilter extends OncePerRequestFilter {
+    @Override
+    protected void doFilterInternal(
+            final HttpServletRequest request,
+            final HttpServletResponse response,
+            final FilterChain filterChain
+    ) throws ServletException, IOException {
+        final ReadableRequestWrapper requestWrapper = new ReadableRequestWrapper(request);
+        final ContentCachingResponseWrapper responseWrapper = new ContentCachingResponseWrapper(response);
+        filterChain.doFilter(requestWrapper, responseWrapper);
+        responseWrapper.copyBodyToResponse();
+    }
+}
