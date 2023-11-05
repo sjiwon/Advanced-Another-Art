a5d42e4d5934e01a1cb29d3af23c33f3a278fe49
SeoJiWon <sjiwon4491@gmail.com>
2023-11-05 20:29:44 +0900

    refactor: AjaxAuthenticationFilter 코드 구조 수정

--- backend/src/main/java/com/sjiwon/anotherart/global/security/LoginRequest.java
+++ backend/src/main/java/com/sjiwon/anotherart/global/security/LoginRequest.java
@@ -1,7 +1,7 @@
 package com.sjiwon.anotherart.global.security;
 
 public record LoginRequest(
-        String loginId,
-        String loginPassword
+        String id,
+        String password
 ) {
 }
