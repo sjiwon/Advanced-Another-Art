791d2f48e17ac09cba153a7f1daab12d1b2ba85c
SeoJiWon <sjiwon4491@gmail.com>
2023-11-05 15:35:45 +0900

    feat: Request/Response 로깅 인터셉터 구현

--- backend/src/main/java/com/sjiwon/anotherart/global/config/AdditionalWebConfiguration.java
+++ backend/src/main/java/com/sjiwon/anotherart/global/config/AdditionalWebConfiguration.java
@@ -1,11 +1,13 @@
 package com.sjiwon.anotherart.global.config;
 
+import com.sjiwon.anotherart.global.interceptor.RequestLogInterceptor;
 import com.sjiwon.anotherart.global.resolver.ExtractPayloadArgumentResolver;
 import com.sjiwon.anotherart.global.resolver.ExtractTokenArgumentResolver;
 import com.sjiwon.anotherart.token.utils.TokenProvider;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
@@ -14,6 +16,14 @@ import java.util.List;
 @RequiredArgsConstructor
 public class AdditionalWebConfiguration implements WebMvcConfigurer {
     private final TokenProvider tokenProvider;
+    private final RequestLogInterceptor requestLogInterceptor;
+
+    @Override
+    public void addInterceptors(final InterceptorRegistry registry) {
+        registry.addInterceptor(requestLogInterceptor)
+                .addPathPatterns("/**")
+                .order(1);
+    }
 
     @Override
     public void addArgumentResolvers(final List<HandlerMethodArgumentResolver> resolvers) {
