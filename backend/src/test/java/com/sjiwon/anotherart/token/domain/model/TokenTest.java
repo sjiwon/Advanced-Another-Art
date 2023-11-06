6341a663540349c0d01ab88f3857e75c0c866423
SeoJiWon <sjiwon4491@gmail.com>
2023-11-06 14:10:14 +0900

    test: TokenTest `@DisplayName` 수정

--- backend/src/test/java/com/sjiwon/anotherart/token/domain/model/TokenTest.java
+++ backend/src/test/java/com/sjiwon/anotherart/token/domain/model/TokenTest.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Test;
 import static com.sjiwon.anotherart.common.utils.TokenUtils.REFRESH_TOKEN;
 import static org.assertj.core.api.Assertions.assertThat;
 
-@DisplayName("Token 도메인 테스트")
+@DisplayName("Token -> 도메인 [Token] 테스트")
 class TokenTest {
     @Test
     @DisplayName("Token을 업데이트한다")
