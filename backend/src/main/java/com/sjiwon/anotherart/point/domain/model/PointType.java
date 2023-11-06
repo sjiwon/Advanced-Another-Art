5e038345a82c3c39c07e92ce3d4329b4c6af9e57
SeoJiWon <sjiwon4491@gmail.com>
2023-11-06 19:28:18 +0900

    refactor: PointType isIncreaseType() 재정의 -> 필드로 수정

--- backend/src/main/java/com/sjiwon/anotherart/point/domain/model/PointType.java
+++ backend/src/main/java/com/sjiwon/anotherart/point/domain/model/PointType.java
@@ -6,25 +6,12 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public enum PointType {
-    CHARGE("포인트 충전") {
-        @Override
-        public boolean isIncreaseType() {
-            return true;
-        }
-    },
-    REFUND("포인트 환불"),
-    PURCHASE("작품 구매"),
-    SOLD("작품 판매") {
-        @Override
-        public boolean isIncreaseType() {
-            return true;
-        }
-    },
+    CHARGE("포인트 충전", true),
+    REFUND("포인트 환불", false),
+    PURCHASE("작품 구매", false),
+    SOLD("작품 판매", true),
     ;
 
     private final String description;
-
-    public boolean isIncreaseType() {
-        return false;
-    }
+    private final boolean increaseType;
 }
