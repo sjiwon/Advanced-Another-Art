d3c83b0c54dd5377dc34f3705035032b7afdeacf
SeoJiWon <sjiwon4491@gmail.com>
2023-11-06 15:15:16 +0900

    test: JwtTokenProviderTest, AuthorizationExtractorTest `@DisplayName` 수정

--- backend/src/test/java/com/sjiwon/anotherart/token/utils/JwtTokenProviderTest.java
+++ backend/src/test/java/com/sjiwon/anotherart/token/utils/JwtTokenProviderTest.java
@@ -1,24 +1,27 @@
 package com.sjiwon.anotherart.token.utils;
 
+import com.sjiwon.anotherart.member.domain.Member;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static com.sjiwon.anotherart.common.fixture.MemberFixture.MEMBER_A;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
 
-@DisplayName("Token [Utils] -> JwtTokenProvider 테스트")
+@DisplayName("Token -> JwtTokenProvider 테스트")
 class JwtTokenProviderTest {
     private static final String SECRET_KEY = "asldfjsadlfjalksjf01jf02j9012f0120f12jf1j29v0saduf012ue101212c01";
-    private static final JwtTokenProvider INVALID_PROVIDER = new JwtTokenProvider(SECRET_KEY, 0L, 0L);
-    private static final JwtTokenProvider VALID_PROVIDER = new JwtTokenProvider(SECRET_KEY, 7200L, 7200L);
-    private static final Long MEMBER_ID = 1L;
+
+    private final JwtTokenProvider invalidProvider = new JwtTokenProvider(SECRET_KEY, 0L, 0L);
+    private final JwtTokenProvider validProvider = new JwtTokenProvider(SECRET_KEY, 7200L, 7200L);
+    private final Member member = MEMBER_A.toMember().apply(1L);
 
     @Test
     @DisplayName("AccessToken과 RefreshToken을 발급한다")
     void issueAccessTokenAndRefreshToken() {
         // when
-        final String accessToken = VALID_PROVIDER.createAccessToken(MEMBER_ID);
-        final String refreshToken = VALID_PROVIDER.createRefreshToken(MEMBER_ID);
+        final String accessToken = validProvider.createAccessToken(member.getId());
+        final String refreshToken = validProvider.createRefreshToken(member.getId());
 
         // then
         assertAll(
@@ -31,22 +34,22 @@ class JwtTokenProviderTest {
     @DisplayName("Token의 Payload를 추출한다")
     void extractTokenPayload() {
         // when
-        final String accessToken = VALID_PROVIDER.createAccessToken(MEMBER_ID);
+        final String accessToken = validProvider.createAccessToken(member.getId());
 
         // then
-        assertThat(VALID_PROVIDER.getId(accessToken)).isEqualTo(MEMBER_ID);
+        assertThat(validProvider.getId(accessToken)).isEqualTo(member.getId());
     }
 
     @Test
     @DisplayName("Token 만료에 대한 유효성을 검증한다")
     void validateTokenExpire() {
         // given
-        final String validToken = VALID_PROVIDER.createAccessToken(MEMBER_ID);
-        final String invalidToken = INVALID_PROVIDER.createAccessToken(MEMBER_ID);
+        final String validToken = validProvider.createAccessToken(member.getId());
+        final String invalidToken = invalidProvider.createAccessToken(member.getId());
 
         // when
-        final boolean actual1 = VALID_PROVIDER.isTokenValid(validToken);
-        final boolean actual2 = INVALID_PROVIDER.isTokenValid(invalidToken);
+        final boolean actual1 = validProvider.isTokenValid(validToken);
+        final boolean actual2 = invalidProvider.isTokenValid(invalidToken);
 
         // then
         assertAll(
@@ -59,10 +62,10 @@ class JwtTokenProviderTest {
     @DisplayName("Token 조작에 대한 유효성을 검증한다")
     void validateTokenManipulation() {
         // given
-        final String forgedToken = VALID_PROVIDER.createAccessToken(MEMBER_ID) + "hacked";
+        final String forgedToken = validProvider.createAccessToken(member.getId()) + "hacked";
 
         // when
-        final boolean actual = VALID_PROVIDER.isTokenValid(forgedToken);
+        final boolean actual = validProvider.isTokenValid(forgedToken);
 
         // then
         assertThat(actual).isFalse();
