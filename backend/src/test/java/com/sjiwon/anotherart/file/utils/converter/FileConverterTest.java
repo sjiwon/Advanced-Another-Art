32e572d8597c25274cdd4357d0aba7c2e5f4025a
SeoJiWon <sjiwon4491@gmail.com>
2023-11-06 16:06:53 +0900

    feat: MultipartFile(Spring) -> RawFileData(POJO) 변환기 구현

---
+++ backend/src/test/java/com/sjiwon/anotherart/file/utils/converter/FileConverterTest.java
@@ -0,0 +1,49 @@
+package com.sjiwon.anotherart.file.utils.converter;
+
+import com.sjiwon.anotherart.file.domain.model.RawFileData;
+import com.sjiwon.anotherart.file.exception.FileErrorCode;
+import com.sjiwon.anotherart.global.exception.AnotherArtException;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
+import static com.sjiwon.anotherart.common.utils.FileMockingUtils.createSingleMockMultipartFile;
+import static com.sjiwon.anotherart.file.domain.model.FileExtension.PNG;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+@DisplayName("FIle -> FileConverter 테스트")
+public class FileConverterTest {
+    @Test
+    @DisplayName("MultipartFile이 비어있으면 예외가 발생한다")
+    void throwExceptionByFIleIsNotUpload() {
+        // given
+        final MultipartFile file = new MockMultipartFile("hello.png", new byte[0]);
+
+        // when - then
+        assertThatThrownBy(() -> FileConverter.convertImageFile(file))
+                .isInstanceOf(AnotherArtException.class)
+                .hasMessage(FileErrorCode.FILE_IS_NOT_UPLOAD.getMessage());
+    }
+
+    @Test
+    @DisplayName("MultipartFile -> RawFileData로 Converting한다")
+    void success() throws IOException {
+        // given
+        final MultipartFile file = createSingleMockMultipartFile("1.png", "image/png");
+
+        // when
+        final RawFileData rawFileData = FileConverter.convertImageFile(file);
+
+        // then
+        assertAll(
+                () -> assertThat(rawFileData.fileName()).isEqualTo("1.png"),
+                () -> assertThat(rawFileData.contenType()).isEqualTo("image/png"),
+                () -> assertThat(rawFileData.extension()).isEqualTo(PNG)
+        );
+    }
+}
