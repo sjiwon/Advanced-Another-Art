61c31a6f09d130b28deb9e5665a68a5a9d6a9b8a
SeoJiWon <sjiwon4491@gmail.com>
2023-11-07 15:42:40 +0900

    refactor: Brief 값으로 ArtType 조회 로직 구현

--- backend/src/main/java/com/sjiwon/anotherart/art/domain/model/ArtType.java
+++ backend/src/main/java/com/sjiwon/anotherart/art/domain/model/ArtType.java
@@ -1,14 +1,26 @@
 package com.sjiwon.anotherart.art.domain.model;
 
+import com.sjiwon.anotherart.art.exception.ArtErrorCode;
+import com.sjiwon.anotherart.global.exception.AnotherArtException;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+
+import java.util.Arrays;
 
 @Getter
 @RequiredArgsConstructor
 public enum ArtType {
-    GENERAL("일반 작품"),
-    AUCTION("경매 작품"),
+    GENERAL("일반 작품", "general"),
+    AUCTION("경매 작품", "auction"),
     ;
 
     private final String description;
+    private final String brief;
+
+    public static ArtType from(final String value) {
+        return Arrays.stream(values())
+                .filter(type -> type.brief.equals(value))
+                .findFirst()
+                .orElseThrow(() -> AnotherArtException.type(ArtErrorCode.INVALID_ART_TYPE));
+    }
 }
