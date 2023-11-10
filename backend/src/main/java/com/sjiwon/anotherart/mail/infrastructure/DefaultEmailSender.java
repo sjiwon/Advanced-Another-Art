a845e44c187d5f912dbdffc156fe5c7cc464f551
SeoJiWon <sjiwon4491@gmail.com>
2023-11-10 17:56:14 +0900

    feat: 이메일 전송 컴포넌트 DefaultEmailSender 구현

---
+++ backend/src/main/java/com/sjiwon/anotherart/mail/infrastructure/DefaultEmailSender.java
@@ -0,0 +1,77 @@
+package com.sjiwon.anotherart.mail.infrastructure;
+
+import com.sjiwon.anotherart.global.exception.AnotherArtException;
+import com.sjiwon.anotherart.global.exception.GlobalErrorCode;
+import com.sjiwon.anotherart.mail.application.adapter.EmailSender;
+import jakarta.mail.internet.InternetAddress;
+import jakarta.mail.internet.MimeMessage;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Profile;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Component;
+import org.thymeleaf.context.Context;
+import org.thymeleaf.spring6.SpringTemplateEngine;
+
+@Slf4j
+@Profile("default")
+@Component
+public class DefaultEmailSender implements EmailSender {
+    private final JavaMailSender mailSender;
+    private final SpringTemplateEngine templateEngine;
+    private final String serviceEmail;
+
+    public DefaultEmailSender(
+            final JavaMailSender mailSender,
+            final SpringTemplateEngine templateEngine,
+            @Value("${spring.mail.username}") final String serviceEmail
+    ) {
+        this.mailSender = mailSender;
+        this.templateEngine = templateEngine;
+        this.serviceEmail = serviceEmail;
+    }
+
+    @Override
+    public void sendAuthCodeForLoginId(final String targetEmail, final String authCode) {
+        final Context context = new Context();
+        context.setVariable("authCode", authCode);
+
+        final String mailBody = templateEngine.process(EmailMetadata.AUTH_TEMPLATE, context);
+        sendMail(
+                EmailMetadata.LOGIN_ID_AUTH_CODE_TITLE,
+                targetEmail,
+                mailBody
+        );
+    }
+
+    @Override
+    public void sendAuthCodeForPassword(final String targetEmail, final String authCode) {
+        final Context context = new Context();
+        context.setVariable("authCode", authCode);
+
+        final String mailBody = templateEngine.process(EmailMetadata.AUTH_TEMPLATE, context);
+        sendMail(
+                EmailMetadata.PASSWORD_AUTH_CODE_TITLE,
+                targetEmail,
+                mailBody
+        );
+    }
+
+    private void sendMail(final String subject, final String email, final String mailBody) {
+        try {
+            final MimeMessage message = mailSender.createMimeMessage();
+            final MimeMessageHelper helper = new MimeMessageHelper(message, true, "UTF-8");
+
+            helper.setSubject(subject);
+            helper.setTo(email);
+            helper.setFrom(new InternetAddress(serviceEmail, "Another Art"));
+            helper.setText(mailBody, true);
+
+            mailSender.send(message);
+        } catch (final Exception e) {
+            log.warn("메일 전송 간 오류 발생...", e);
+            throw AnotherArtException.type(GlobalErrorCode.INTERNAL_SERVER_ERROR);
+        }
+    }
+}
