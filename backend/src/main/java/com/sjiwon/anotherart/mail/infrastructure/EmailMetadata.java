21f636b84976ed6400547f382f5f3b202c69321f
SeoJiWon <sjiwon4491@gmail.com>
2023-11-10 17:07:22 +0900

    feat: 이메일 전송 메타데이터 정보 관리 인터페이스 구현

---
+++ backend/src/main/java/com/sjiwon/anotherart/mail/infrastructure/EmailMetadata.java
@@ -0,0 +1,6 @@
+package com.sjiwon.anotherart.mail.infrastructure;
+
+public interface EmailMetadata {
+    String LOGIN_ID_AUTH_CODE_SUBJECT = "아이디 찾기 인증번호 메일입니다.";
+    String PASSWORD_AUTH_CODE_SUBJECT = "비밀번호 재설정 인증번호 메일입니다.";
+}
