be8d23b158a66da236b6d11cdb01bb5d8689934d
SeoJiWon <sjiwon4491@gmail.com>
2023-11-20 14:33:47 +0900

    refactor: 로그인 관련 Token 처리 방식 수정
    - JSON -> Header로 응답받기

--- frontend/src/apis/axios.js
+++ frontend/src/apis/axios.js
@@ -1,85 +1,33 @@
 import Axios from 'axios'
-import {accessTokenProvider, refreshTokenProvider} from '@/utils/token'
 
 const axios = Axios.create({
-  baseURL: process.env.VUE_APP_API_URL,
+  baseURL: process.env.SERVER_API_URL,
   headers: {
     'Content-Type': 'application/json;charset=UTF-8',
     accept: 'application/json'
   },
   withCredentials: true
 })
-
-const axiosWithAccessToken = Axios.create({
-  baseURL: process.env.VUE_APP_API_URL,
-  headers: {
-    'Content-Type': 'application/json;charset=UTF-8',
-    accept: 'application/json'
-  },
-  withCredentials: true
-})
-
-const axiosWithRefreshToken = Axios.create({
-  baseURL: process.env.VUE_APP_API_URL,
-  headers: {
-    'Content-Type': 'application/json;charset=UTF-8',
-    accept: 'application/json'
-  },
-  withCredentials: true
-})
-
-// Request Interceptor
-axiosWithAccessToken.interceptors.request.use(
-  config => {
-    const accessToken = accessTokenProvider.get()
-
-    if (config.headers && accessToken) {
-      config.headers.Authorization = `Bearer ${accessToken}`
-    }
-
-    return config
-  },
-  (error) => {
-    return Promise.reject(error)
-  }
-)
-
-axiosWithRefreshToken.interceptors.request.use(
-  config => {
-    const refreshToken = refreshTokenProvider.get()
-
-    if (config.headers && refreshToken) {
-      config.headers.Authorization = `Bearer ${refreshToken}`
-    }
-
-    return config
-  },
-  (error) => {
-    return Promise.reject(error)
-  }
-)
 
 // Response Interceptor
-axiosWithAccessToken.interceptors.response.use(
+axios.interceptors.response.use(
   response => {
     return response
   },
   error => {
     const originalConfig = error.config
 
-    if (error.response.data.errorCode === 'AUTH_005') { // 유효하지 않은 토큰
-      axiosWithRefreshToken // 토큰 재발급
+    if (error.response.data.errorCode === 'TOKEN_001') { // 유효하지 않은 토큰
+      axios // 토큰 재발급
         .post('/api/token/reissue')
         .then(response => {
-          accessTokenProvider.set(response.data.accessToken)
-          refreshTokenProvider.set(response.data.refreshToken)
-          return axiosWithAccessToken.request(originalConfig)
+          return axios.request(originalConfig)
         })
         .catch(() => {
           alert('권한이 없습니다.\n로그인 페이지로 이동합니다.')
           window.location.href = '/login'
         })
-    } else if (error.response.data.errorCode === 'AUTH_003') {
+    } else if (error.response.data.errorCode === 'AUTH_003' || error.response.data.errorCode === 'AUTH_004') {
       alert('권한이 없습니다.\n로그인 페이지로 이동합니다.')
       window.location.href = '/login'
     } else {
@@ -89,7 +37,5 @@ axiosWithAccessToken.interceptors.response.use(
 )
 
 export {
-  axios,
-  axiosWithAccessToken,
-  axiosWithRefreshToken
+  axios
 }
--- frontend/src/store/modules/member.js
+++ frontend/src/store/modules/member.js
@@ -5,16 +5,12 @@ const memberStore = {
     loginSuccess: false,
     memberId: '',
     nickname: '',
-    accessToken: '',
-    refreshToken: ''
   },
 
   getters: {
     isAuthenticated: state => state.loginSuccess,
     getMemberId: state => state.memberId,
     getMemberNickname: state => state.nickname,
-    getAccessToken: state => state.accessToken,
-    getRefreshToken: state => state.refreshToken
   },
 
   mutations: {
@@ -22,21 +18,11 @@ const memberStore = {
       state.loginSuccess = true
       state.memberId = payload.memberId
       state.nickname = payload.nickname
-      state.accessToken = payload.accessToken
-      state.refreshToken = payload.refreshToken
-    },
-    updateAccessToken: (state, accessToken) => {
-      state.accessToken = accessToken
-    },
-    updateRefreshToken: (state, refreshToken) => {
-      state.refreshToken = refreshToken
     },
     reset: (state) => {
       state.loginSuccess = false
       state.memberId = ''
       state.nickname = ''
-      state.accessToken = ''
-      state.refreshToken = ''
     }
   }
 }
