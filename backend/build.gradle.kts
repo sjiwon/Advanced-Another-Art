34b5949cff4f0b7e0f9d6326271a2f96ae62635d
sjiwon <sjiwon4491@gmail.com>
2024-03-22 15:36:27 +0900

    chore: jOOQ + Flyway + TestContainers build.gradle.kts 설정

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -1,9 +1,17 @@
+import nu.studer.gradle.jooq.JooqEdition
+import nu.studer.gradle.jooq.JooqGenerate
+import org.jooq.meta.jaxb.ForcedType
+import org.jooq.meta.jaxb.Logging
 import org.springframework.boot.gradle.tasks.bundling.BootJar
+import org.testcontainers.containers.MySQLContainer
+import org.testcontainers.utility.DockerImageName
 
 plugins {
     java
     id("org.springframework.boot")
     id("io.spring.dependency-management")
+    id("org.flywaydb.flyway")
+    id("nu.studer.jooq")
 }
 
 group = "${property("projectGroup")}"
@@ -33,15 +41,16 @@ dependencies {
 
     // Data
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-    runtimeOnly("com.mysql:mysql-connector-j")
-    implementation("org.flywaydb:flyway-core")
-    implementation("org.flywaydb:flyway-mysql")
+    runtimeOnly("com.mysql:mysql-connector-j:${property("mysqlVersion")}")
+    implementation("org.flywaydb:flyway-core:${property("flywayVersion")}")
+    implementation("org.flywaydb:flyway-mysql:${property("flywayVersion")}")
 
     // Query Builder
-    implementation("com.querydsl:querydsl-jpa:${property("queryDslVersion")}:jakarta")
-    annotationProcessor("com.querydsl:querydsl-apt:${property("queryDslVersion")}:jakarta")
-    annotationProcessor("jakarta.annotation:jakarta.annotation-api")
-    annotationProcessor("jakarta.persistence:jakarta.persistence-api")
+    implementation("org.springframework.boot:spring-boot-starter-jooq")
+    jooqGenerator("mysql:mysql-connector-java:${property("mysqlVersion")}")
+    jooqGenerator("org.jooq:jooq:${property("jooqVersion")}")
+    jooqGenerator("org.jooq:jooq-codegen:${property("jooqVersion")}")
+    jooqGenerator("org.jooq:jooq-meta:${property("jooqVersion")}")
 
     // Redis
     implementation("org.springframework.boot:spring-boot-starter-data-redis")
@@ -51,13 +60,9 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-mail")
 
     // Cloud Infra
-    listOf(
-        "spring-cloud-aws-starter",
-        "spring-cloud-aws-starter-s3",
-        "spring-cloud-aws-starter-ses",
-    ).forEach {
-        implementation("io.awspring.cloud:$it:${property("awspringVersion")}")
-    }
+    implementation("io.awspring.cloud:spring-cloud-aws-starter:${property("awspringVersion")}")
+    implementation("io.awspring.cloud:spring-cloud-aws-starter-s3:${property("awspringVersion")}")
+    implementation("io.awspring.cloud:spring-cloud-aws-starter-ses:${property("awspringVersion")}")
     implementation("ca.pjer:logback-awslogs-appender:${property("cloudWatchLogAppenderVersion")}")
 
     // Log & Monitoring
@@ -65,9 +70,9 @@ dependencies {
     runtimeOnly("io.micrometer:micrometer-registry-prometheus")
 
     // JWT
-    implementation("io.jsonwebtoken:jjwt-api:${property("jwtTokenVersion")}")
-    runtimeOnly("io.jsonwebtoken:jjwt-impl:${property("jwtTokenVersion")}")
-    runtimeOnly("io.jsonwebtoken:jjwt-jackson:${property("jwtTokenVersion")}")
+    implementation("io.jsonwebtoken:jjwt-api:${property("jjwtVersion")}")
+    runtimeOnly("io.jsonwebtoken:jjwt-impl:${property("jjwtVersion")}")
+    runtimeOnly("io.jsonwebtoken:jjwt-jackson:${property("jjwtVersion")}")
 
     // Lombok
     compileOnly("org.projectlombok:lombok")
@@ -90,35 +95,124 @@ dependencies {
     // Test
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.springframework.security:spring-security-test")
+    testImplementation("org.flywaydb.flyway-test-extensions:flyway-spring-test:${property("flywayTestExtensionVersion")}")
 
     // TestContainers
     testImplementation("org.springframework.boot:spring-boot-testcontainers")
-    testImplementation("org.testcontainers:junit-jupiter")
-
-    // TestContainers + RDB(MySQL)
-    testImplementation("org.testcontainers:mysql")
-    testImplementation("org.flywaydb.flyway-test-extensions:flyway-spring-test:${property("flywayTestExtensionVersion")}")
+    testImplementation("org.testcontainers:junit-jupiter:${property("testContainersVersion")}")
+    testImplementation("org.testcontainers:mysql:${property("testContainersVersion")}")
 }
 
 tasks.withType<Test> {
     useJUnitPlatform()
 }
 
-// QueryDsl QClass
-val queryDslTypeDir: String = "src/main/generated"
-
-tasks.withType<JavaCompile>().configureEach {
-    options.generatedSourceOutputDirectory.set(file(queryDslTypeDir))
-}
-
-sourceSets {
-    getByName("main").java.srcDirs(queryDslTypeDir)
-}
-
-tasks.named("clean") {
-    doLast {
-        file(queryDslTypeDir).deleteRecursively()
+// jOOQ
+val mysqlContainer = tasks.register("mysqlContainer") {
+    val container = MySQLContainer<Nothing>(DockerImageName.parse("mysql:8.0.33")).apply {
+        withDatabaseName("another_art")
+        withUsername("root")
+        withPassword("1234")
+        start()
     }
+
+    extra.set("jdbcUrl", container.jdbcUrl)
+    extra.set("username", container.username)
+    extra.set("password", container.password)
+    extra.set("databaseName", container.databaseName)
+    extra.set("container", container)
+}
+
+val mysqlJdbcUrl = mysqlContainer.get().extra["jdbcUrl"].toString()
+val mysqlUsername = mysqlContainer.get().extra["username"].toString()
+val mysqlPassword = mysqlContainer.get().extra["password"].toString()
+val mysqlDatabaseName = mysqlContainer.get().extra["databaseName"].toString()
+val container = mysqlContainer.get().extra["container"] as MySQLContainer<*>
+
+buildscript {
+    dependencies {
+        classpath("mysql:mysql-connector-java:${property("mysqlVersion")}")
+        classpath("org.flywaydb:flyway-mysql:${property("flywayVersion")}")
+        classpath("org.testcontainers:mysql:${property("testContainersVersion")}")
+    }
+
+    configurations["classpath"].resolutionStrategy.eachDependency {
+        if (requested.group.startsWith("org.jooq") && requested.name.startsWith("jooq")) {
+            useVersion("${property("jooqVersion")}")
+        }
+    }
+}
+
+flyway {
+    locations = arrayOf("filesystem:src/main/resources/db/migration")
+    url = mysqlJdbcUrl
+    user = mysqlUsername
+    password = mysqlPassword
+}
+
+jooq {
+    version.set("${property("jooqVersion")}")
+    edition.set(JooqEdition.OSS)
+
+    configurations {
+        create("main") {
+            jooqConfiguration.apply {
+                logging = Logging.WARN
+                jdbc.apply {
+                    driver = "com.mysql.cj.jdbc.Driver"
+                    url = mysqlJdbcUrl
+                    user = mysqlUsername
+                    password = mysqlPassword
+                }
+                generator.apply {
+                    name = "org.jooq.codegen.DefaultGenerator"
+                    database.apply {
+                        name = "org.jooq.meta.mysql.MySQLDatabase"
+                        inputSchema = mysqlDatabaseName
+                        forcedTypes.addAll(
+                            arrayOf(
+                                ForcedType()
+                                    .withName("varchar")
+                                    .withIncludeExpression(".*")
+                                    .withIncludeTypes("JSONB?"),
+                                ForcedType()
+                                    .withName("varchar")
+                                    .withIncludeExpression(".*")
+                                    .withIncludeTypes("INET")
+                            ).toList()
+                        )
+                    }
+                    generate.apply {
+                        isDaos = true
+                        isDeprecated = false
+                        isRecords = true
+                        isImmutablePojos = true
+                        isFluentSetters = true
+                        isJavaTimeTypes = true
+                    }
+                    target.apply {
+                        directory = "src/generated"
+                        packageName = "com.sjiwon.anotherart.jooq"
+                        encoding = "UTF-8"
+                    }
+                    strategy.name = "org.jooq.codegen.example.JPrefixGeneratorStrategy"
+                }
+            }
+        }
+    }
+}
+
+tasks.named<JooqGenerate>("generateJooq") {
+    dependsOn("mysqlContainer")
+    dependsOn("flywayMigrate")
+
+    inputs.files(fileTree("src/main/resources/db/migration"))
+        .withPropertyName("migration")
+        .withPathSensitivity(PathSensitivity.RELATIVE)
+
+    allInputsDeclared.set(true)
+    outputs.cacheIf { true }
+    doLast { container.stop() }
 }
 
 // Copy Submodule
--- backend/settings.gradle.kts
+++ backend/settings.gradle.kts
@@ -3,12 +3,16 @@ rootProject.name = "Advanced-Another-Art"
 pluginManagement {
     val springBootVersion: String by settings
     val springDependencyManagementVersion: String by settings
+    val flywayVersion: String by settings
+    val jooqPluginVersion: String by settings
 
     resolutionStrategy {
         eachPlugin {
             when (requested.id.id) {
                 "org.springframework.boot" -> useVersion(springBootVersion)
                 "io.spring.dependency-management" -> useVersion(springDependencyManagementVersion)
+                "org.flywaydb.flyway" -> useVersion(flywayVersion)
+                "nu.studer.jooq" -> useVersion(jooqPluginVersion)
             }
         }
     }
